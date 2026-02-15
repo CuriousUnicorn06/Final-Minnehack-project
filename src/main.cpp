9c37548c07a8becdd09f983765af0c190012809d
Avery <avery.lenderman@gmail.com>
2026-02-15 00:23:37 -0600

    f

--- src/main.cpp
+++ src/main.cpp
@@ -1,68 +1,46 @@
+#include <Arduino.h>
 #include <WiFi.h>
 #include <WebServer.h>
 
-// ---- CHANGE THESE ----
-const char* ssid = "alaynas iPhone";
+const char* ssid     = "alayna iPhone";
 const char* password = "abcdefghij";
-
-// ---- PINS ----
-const int LED_PIN = 2;      // Built‑in LED on many ESP32 boards
-const int BUTTON_PIN = 14;  // Change to whatever pin your button uses
 
 WebServer server(80);
 
 // ---- WEBPAGE ----
-// ---- WEBPAGE ----
-String htmlPage = R"rawliteral(
+const char htmlPage[] PROGMEM = R"rawliteral(
 <!DOCTYPE html>
 <html>
 <head>
   <title>To-Do Competition</title>
 </head>
 <body>
-
   <h2>Pick a User</h2>
 
   <form action="/submit" method="GET">
-    
-    <label>
-      <input type="radio" name="user" value="User1" required>
-      User 1
-    </label><br>
-
-    <label>
-      <input type="radio" name="user" value="User2">
-      User 2
-    </label><br><br>
+    <label><input type="radio" name="user" value="User1" required> User 1</label><br>
+    <label><input type="radio" name="user" value="User2"> User 2</label><br><br>
 
     <h3>Enter To-Do List:</h3>
-    <textarea name="todo" rows="6" cols="30"
-      placeholder="Ex: 
-- Homework
-- Gym
-- Laundry"></textarea><br><br>
+    <textarea name="todo" rows="8" cols="32" placeholder="- Homework&#10;- Gym&#10;- Laundry"></textarea><br><br>
 
     <input type="submit" value="Save List">
-
   </form>
-
 </body>
 </html>
 )rawliteral";
 
-
 void handleRoot() {
   server.send(200, "text/html", htmlPage);
-
 }
+
 void handleSubmit() {
   String user = server.arg("user");
   String todo = server.arg("todo");
 
   Serial.println("----- NEW LIST -----");
   Serial.println("User: " + user);
-  Serial.println("To-do:");
-  Serial.println(todo);
+  Serial.println("To-do:\n" + todo);
 
   String response = "<h2>Saved for " + user + "!</h2>";
   response += "<pre>" + todo + "</pre>";
@@ -73,49 +51,34 @@ void handleSubmit() {
 
 void setup() {
   Serial.begin(115200);
-  delay(1000);
-  Serial.println("this is new and works");
-  server.on("/", handleRoot);
-  server.on("/submit", handleSubmit);
-  server.begin();
-  // ---- LED ----
-  pinMode(LED_PIN, OUTPUT);
+  delay(200);
+  Serial.println("\nBooting...");
 
-  // ---- BUTTON ----
-  pinMode(BUTTON_PIN, INPUT_PULLUP);
-
-  // ---- WIFI ----
-  Serial.println("Connecting to WiFi...");
+  WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
 
+  Serial.print("Connecting to WiFi");
+  unsigned long start = millis();
   while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
+    delay(300);              // ✅ prevents watchdog resets
     Serial.print(".");
+    if (millis() - start > 20000) {   // 20s timeout so it doesn't hang forever
+      Serial.println("\nWiFi failed. Rebooting...");
+      ESP.restart();
+    }
   }
 
   Serial.println("\nConnected!");
   Serial.print("ESP32 IP Address: ");
   Serial.println(WiFi.localIP());
 
-  // ---- WEB SERVER ----
   server.on("/", handleRoot);
+  server.on("/submit", handleSubmit);
   server.begin();
   Serial.println("Web server started!");
 }
 
 void loop() {
   server.handleClient();
-
-  // ---- LED BLINK TEST ----
-  digitalWrite(LED_PIN, HIGH);
-  delay(500);
-  digitalWrite(LED_PIN, LOW);
-  delay(500);
-
-  // ---- BUTTON TEST ----
-  if (digitalRead(BUTTON_PIN) == LOW) {  // Button pressed
-    Serial.println("Button pressed!");
-    delay(300); // Debounce
-
-  }
+  delay(2); // ✅ tiny yield to keep system happy
 }
