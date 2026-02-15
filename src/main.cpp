c8f59bdec5da884214376e840accfd4df7b8aeda
Avery <avery.lenderman@gmail.com>
2026-02-15 01:54:44 -0600

    g

--- src/main.cpp
+++ src/main.cpp
@@ -8,19 +8,46 @@ const char* password = "abcdefghij";
 WebServer server(80);
 
 // ---- WEBPAGE ----
-String htmlPage = R"rawliteral(
- <!DOCTYPE html>
-  <html> <head> 
-  <title>ESP32 Test</title> 
-  </head>
-   <body>
-   <h1>ESP32 is working!</h1> 
-   <p>If you can see this page, the web server works.</p>
-    </body>
-     </html> 
-     )rawliteral";
-      void handleRoot() { server.send(200, "text/html", htmlPage); }
+const char htmlPage[] PROGMEM = R"rawliteral(
+<!DOCTYPE html>
+<html>
+<head>
+  <title>To-Do Competition</title>
+</head>
+<body>
+  <h2>Pick a User</h2>
 
+  <form action="/submit" method="GET">
+    <label><input type="radio" name="user" value="User1" required> User 1</label><br>
+    <label><input type="radio" name="user" value="User2"> User 2</label><br><br>
+
+    <h3>Enter To-Do List:</h3>
+    <textarea name="todo" rows="8" cols="32" placeholder="- Homework&#10;- Gym&#10;- Laundry"></textarea><br><br>
+
+    <input type="submit" value="Save List">
+  </form>
+</body>
+</html>
+)rawliteral";
+
+void handleRoot() {
+  server.send(200, "text/html", htmlPage);
+}
+
+void handleSubmit() {
+  String user = server.arg("user");
+  String todo = server.arg("todo");
+
+  Serial.println("----- NEW LIST -----");
+  Serial.println("User: " + user);
+  Serial.println("To-do:\n" + todo);
+
+  String response = "<h2>Saved for " + user + "!</h2>";
+  response += "<pre>" + todo + "</pre>";
+  response += "<a href='/'>Go back</a>";
+
+  server.send(200, "text/html", response);
+}
 
 void setup() {
   Serial.begin(115200);
@@ -35,12 +62,10 @@ void setup() {
   while (WiFi.status() != WL_CONNECTED) {
     delay(300);              // ✅ prevents watchdog resets
     Serial.print(".");
-    if (millis() - start > 30000) {
-  Serial.println("\nWiFi failed.");
-  Serial.print("WiFi.status() = "); Serial.println(WiFi.status());
-  Serial.print("RSSI = "); Serial.println(WiFi.RSSI());
-  break;  // stop reboot loop so you can read info
-}
+    if (millis() - start > 20000) {   // 20s timeout so it doesn't hang forever
+      Serial.println("\nWiFi failed. Rebooting...");
+      ESP.restart();
+    }
   }
 
   Serial.println("\nConnected!");
@@ -48,6 +73,7 @@ void setup() {
   Serial.println(WiFi.localIP());
 
   server.on("/", handleRoot);
+  server.on("/submit", handleSubmit);
   server.begin();
   Serial.println("Web server started!");
 }
